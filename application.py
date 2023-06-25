632d86ad4a5bf39309244014e8bf83f054c199e6
Axel Jacobsen <axelnj44@gmail.com>
2023-06-24 18:44:18 -0700

    signup?

--- application.py
+++ application.py
@@ -1,13 +1,34 @@
 #! /usr/bin/env python3
 
+# import boto3
+
 from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 application = Flask(__name__)
+application.secret_key = "i am a secret key"
 
 @application.route('/')
-@application.route('/<rest>')
-def home(rest=""):
-    return render_template('index.html', rest=rest)
+def home():
+    return render_template('index.html')
+
+
+@application.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+
+        # Here, you would usually store the user data in a database
+        # For simplicity, we'll use flash messages
+        flash(f'Account created for {username}!', 'success')
+
+        return redirect(url_for('signup'))
+
+    return render_template('signup.html')
+
+
 
 if __name__ == '__main__':
     application.run(debug=False)
