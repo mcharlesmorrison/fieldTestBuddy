245de8c3b1fcf8e9984ff906f10056e46653b9a1
Axel Jacobsen <axelnj44@gmail.com>
2023-06-24 21:15:04 -0700

    sketches of brunswick east

--- ftb/application.py
+++ ftb/application.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-# import boto3
+from argon2 import PasswordHasher
 
 from flask import Flask, render_template
 from flask import Flask, render_template, request, redirect, url_for, flash
@@ -16,10 +16,16 @@ def home():
 
 @application.route("/signup", methods=["GET", "POST"])
 def signup():
+    """
+    TODO
+        - impl db
+    """
     if request.method == "POST":
         username = request.form["username"]
         email = request.form["email"]
-        password = request.form["password"]
+
+        ph = PasswordHasher()
+        hashed_password = ph.hash(request.form["password"])
 
         # Here, you would usually store the user data in a database
         # For simplicity, we'll use flash messages
@@ -30,5 +36,29 @@ def signup():
     return render_template("signup.html")
 
 
+@application.route("/login", methods=["GET", "POST"])
+def login():
+    """
+    TODO
+        - impl db
+    """
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+
+        ph = PasswordHasher()
+        hash = "1234"  # get hash from db
+        ph.verify(hash, password)
+
+        # Here, you would usually check the user's password against
+        # the hashed password in your database
+        # For simplicity, we'll use flash messages
+        flash(f"Logged in as {username}!", "success")
+
+        return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
 if __name__ == "__main__":
-    application.run(debug=False)
+    application.run(debug=True)
